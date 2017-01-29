2674f663490a1e58db59992ee74a7c12e1a156de
Paul <P.Chudzik@stud.elka.pw.edu.pl>
2017-01-29 01:34:58 +0100

    args4j

--- src/main/scala/pl/edu/pw/elka/mbi/cli/VariantCaller.scala
+++ src/main/scala/pl/edu/pw/elka/mbi/cli/VariantCaller.scala
@@ -11,87 +11,88 @@ import org.bdgenomics.adam.rdd.read.AlignmentRecordRDD
 import org.bdgenomics.adam.rdd.variation.VariantContextRDD
 import org.bdgenomics.formats.avro.{AlignmentRecord, Sample}
 import org.bdgenomics.utils.instrumentation.Metrics
+import org.kohsuke.args4j.{CmdLineParser, Argument, Option}
 import pl.edu.pw.elka.mbi.core.instrumentation.Timers._
-import pl.edu.pw.elka.mbi.core.model.{Allele, ReferenceAllele}
+import pl.edu.pw.elka.mbi.core.model.Allele
 import pl.edu.pw.elka.mbi.core.preprocessing.Preprocessor
 import pl.edu.pw.elka.mbi.core.reads.{ReferenceSequence, VariantDiscovery}
 import pl.edu.pw.elka.mbi.core.variants.ThresholdCaller
 
 object VariantCaller {
-  private val ARGS = Map(
-    "DEBUG" -> ((arg: String) => arg.toBoolean),
-    "reference" -> ((arg: String) => arg),
-    "alignment" -> ((arg: String) => arg),
-    "out" -> ((arg: String) => arg),
-    "mapqThreshold" -> ((arg: String) => arg),
-    "homozygousThreshold" -> ((arg: String) => arg.toInt),
-    "heterozygousThreshold" -> ((arg: String) => arg.toInt)
-    )
-  private var args = Map[String, Any]()
 
-  def debug(text: String) = {
-    if(args.get("DEBUG").isDefined) {
+  @Argument(usage = "Path to the reference file", metaVar = "Reference", required = true, index = 0)
+  private var referenceFile: String = _
+
+  @Argument(usage = "Path to the alignment file", metaVar = "Alignment", required = true, index = 1)
+  private var alignmentFile: String = _
+
+  @Argument(usage = "Path to the output vcf file", metaVar = "Output", required = true, index = 2)
+  private var outputFile: String = _
+
+  @Option(name = "-debug", usage = "Turn on debug logs")
+  private var debug: Boolean = false
+
+  @Option(name = "-homozygousThreshold", usage = "Set the homozygous threshold")
+  private var homozygousThreshold: Double = 0.8
+
+  @Option(name = "-heterozygousThreshold", usage = "Set the heterozygous threshold")
+  private var heterozygousThreshold: Double = 0.2
+
+  @Option(name = "-mapq", usage = "Filter reads based on mapping quality")
+  private var mapqFilter: Int = 0
+
+  def debugLog(text: String) = {
+    if(debug) {
       println(text)
     }
   }
 
   def debugRDD(text: RDD[String]) = {
-    text foreach debug
+    text foreach debugLog
   }
 
-  def main(args: Array[String]) {
-    this.args = getArgs(args)
-
-    if (this.args.get("reference").isEmpty ||
-        this.args.get("alignment").isEmpty ||
-        this.args.get("out").isEmpty) {
-      System.err.println("Specify reference file, read file and vcf file")
-      System.exit(1)
-    }
-
-    this.args = getArgs(args)
+  def apply(args: Array[String]) = {
+    new CmdLineParser(this).parseArgument(args: _*)
 
     val conf = new SparkConf()
-                     .setAppName("Variant Caller")
-                     .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-                     .set("spark.kryo.registrator", "org.bdgenomics.adam.serialization.ADAMKryoRegistrator")
-                     .set("spark.kryo.referenceTracking", "true")
-                     //.set("spark.kryo.registrationRequired", "true")
+                      .setAppName("Variant Caller")
+                      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+                      .set("spark.kryo.registrator", "org.bdgenomics.adam.serialization.ADAMKryoRegistrator")
+                      .set("spark.kryo.referenceTracking", "true")
+                    //.set("spark.kryo.registrationRequired", "true")
 
     val sc = new SparkContext(conf)
 
     Metrics.initialize(sc)
 
     val alignments: AlignmentRecordRDD = LoadReads.time {
-      sc.loadAlignments(this.args("alignment").toString)
+      sc.loadAlignments(alignmentFile)
     }
     val sequence: NucleotideContigFragmentRDD = LoadReference.time {
-      sc.loadSequences(this.args("reference").toString)
+      sc.loadSequences(referenceFile)
     }
 
-    val rdd: RDD[AlignmentRecord] = new Preprocessor(alignments)
-                                          //.realignIndels()
-                                          //.filterByMappingQuality(0)
-                                          .getReads
+    val reads: RDD[AlignmentRecord] = new Preprocessor(alignments)
+                                            //.realignIndels()
+                                            //.filterByMappingQuality(0)
+                                            .getReads
 
     val reference: RDD[(ReferencePosition, Allele)] = ReferenceSequence(sequence.rdd)
 
-    val discovered = VariantDiscovery(rdd, reference)
+    val discovered = VariantDiscovery(reads, reference)
 
     val variants = ThresholdCaller(discovered)
 
-    VariantContextRDD(variants,
-                      sequence.sequences,
-                      Seq(new Sample("sample","sample",null))).saveAsVcf(this.args("out").toString, asSingleFile = true)
+    VariantContextRDD(
+      variants,
+      sequence.sequences,
+      Seq(new Sample("sample", "sample", null))
+    ).saveAsVcf(outputFile, asSingleFile = true)
 
     Metrics.print(new PrintWriter(System.out), None)
   }
 
-  private def getArgs(args: Array[String]) = {
-    args
-      .map(_.split('='))
-      .foldLeft(Map[String, Any]()) {
-        case (map, arg) => map + (arg(0) -> ARGS(arg(0))(arg(1)))
-      }
+  def main(args: Array[String]) {
+    VariantCaller(args)
   }
 }
